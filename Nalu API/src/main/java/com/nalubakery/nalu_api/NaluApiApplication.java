b223074c2b7a25a9b203440610aab3673f641305
Carlos Alvarado <carlosalvarado310.ca@gmail.com>
2024-03-08 11:05:54 -0600

    Initial Nalu API project

---
+++ Nalu API/src/main/java/com/nalubakery/nalu_api/NaluApiApplication.java
@@ -0,0 +1,13 @@
+package com.nalubakery.nalu_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaluApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaluApiApplication.class, args);
+    }
+
+}
---
+++ Nalu API/src/test/java/com/nalubakery/nalu_api/NaluApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nalubakery.nalu_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaluApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
